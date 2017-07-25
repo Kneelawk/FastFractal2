d8739197fe63feb2745db83a6df12a1dc5c27c4b
Kneelawk <jedidiah.pommert@gmail.com>
2017-07-25 00:28:24 -0700

    Make sure launcher jar works

--- src/launcher/java/org/kneelawk/fastfractal2/FastFractal2Launcher.java
+++ src/launcher/java/org/kneelawk/fastfractal2/FastFractal2Launcher.java
@@ -7,7 +7,7 @@ import org.kneelawk.cpcontrol.CPControl3;
 public class FastFractal2Launcher {
 
 	public static void main(String[] args) {
-		CPControl3 cp = new CPControl3("org.kneelwak.fastfractal2.FastFractal2");
+		CPControl3 cp = new CPControl3("org.kneelawk.fastfractal2.FastFractal2");
 		cp.addExtractingFromFileLibrary(CPControl3.ME)
 				.addLibrary("application", new CPControl3.DirectoryEntryFilter("app"), CPControl3.ALWAYS_DELETE)
 				.addLibrary("libraries", new CPControl3.DirectoryEntryFilter("libs"), CPControl3.ALWAYS_DELETE);
